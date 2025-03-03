3127eefaf24fdabc984674e4e8d06d6307a8fea5
soumya <pandey.a.soumya@gmail.com>
2025-03-03 16:44:55 +0530

    test files

---
+++ new/test.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "FastAPI is working!"}
+
+@app.get("/ping")
+async def ping():
+    return {"ping": "pong"}
